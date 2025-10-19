5a1790d9f4ddd8ccc063e2cb0b872e0464283711
mjelodar <jelodarian.92@gmail.com>
2025-10-19 15:31:50 +0330

    commit for 15 min rule

--- src/main/java/com/snapp/expense_tracker/ExpenseTrackerApplication.java
+++ src/main/java/com/snapp/expense_tracker/ExpenseTrackerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+        "com.snapp.expense_tracker.user.domain",
+        "com.snapp.expense_tracker.cost.domain"
+})
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/snapp/expense_tracker/auth/domin/User.java
@@ -0,0 +1,5 @@
+package com.snapp.expense_tracker.auth.domin;
+
+@Table
+public class User {
+}
