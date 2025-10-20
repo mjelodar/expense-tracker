8e11709eb0676d064689542184ca0dee903acf00
mjelodar <jelodarian.92@gmail.com>
2025-10-20 15:11:06 +0330

    commit for 15 min rule,

--- src/main/java/com/snapp/expense_tracker/user/util/JWTUtil.java
+++ src/main/java/com/snapp/expense_tracker/user/util/JWTUtil.java
@@ -1,8 +1,20 @@
 package com.snapp.expense_tracker.user.util;
 
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Component
 public class JWTUtil {
-
+    String generateToken(String username, Long userId, long expiresIn) {
+        Date expiration = new Date(new Date().getTime() + (expiresIn * 1000));
+        return Jwts.builder().
+                claim("userId", userId).
+                claim("username", username).
+                claim("expireAt", expiration).
+                expiration(expiration.)
+    }
 }
