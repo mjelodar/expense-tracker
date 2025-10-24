cfeb46a4bc7515356b3f4ce03300ffe0fa807c89
mjelodar <jelodarian.92@gmail.com>
2025-10-24 09:05:32 +0330

    use distributed lock

--- src/main/java/com/snapp/expense_tracker/report/scheduler/CheckExpiredRuleScheduler.java
+++ src/main/java/com/snapp/expense_tracker/report/scheduler/CheckExpiredRuleScheduler.java
@@ -3,33 +3,37 @@ package com.snapp.expense_tracker.report.scheduler;
 import com.snapp.expense_tracker.common.enums.TimeUnit;
 import com.snapp.expense_tracker.report.repository.RuleRepository;
 import com.snapp.expense_tracker.report.service.RuleService;
+import org.redisson.api.RedissonClient;
+import org.redisson.api.RLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 @Component
 public class CheckExpiredRuleScheduler {
     private final RuleRepository ruleRepository;
     private final RuleService ruleService;
-    private Lock lock;
+    private final RedissonClient redissonClient;;
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public CheckExpiredRuleScheduler(RuleRepository ruleRepository, RuleService ruleService) {
+    public CheckExpiredRuleScheduler(RuleRepository ruleRepository,
+                                     RuleService ruleService,
+                                     RedissonClient redissonClient) {
         this.ruleRepository = ruleRepository;
         this.ruleService = ruleService;
+        this.redissonClient = redissonClient;
     }
 
     @Scheduled(cron = "0 0 0 * * *")
     public synchronized void checkDailyRules() {
-        lock = new ReentrantLock();
+        RLock lock = redissonClient.getLock("DAILY");
         if (lock.tryLock()) {
             try {
                 ruleRepository.findByTimeUnitAndExpirationAtBefore(TimeUnit.DAY, LocalDateTime.now()).
+                        parallelStream().
                         forEach(ruleService::notifyExpiredRule);
             }catch (Exception e){
                 logger.error("Error in daily expired rule,", e);
@@ -39,12 +43,14 @@ public class CheckExpiredRuleScheduler {
         }
     }
 
-    @Scheduled(cron = "0 0 0 * * 0")
+    //TODO think about better way to find weekly/monthly record that needs to be notified
+    @Scheduled(cron = "0 0 */23 * * *")
     public synchronized void checkWeeklyRules() {
-        lock = new ReentrantLock();
+        RLock lock = redissonClient.getLock("WEEKLY");
         if (lock.tryLock()) {
             try {
                 ruleRepository.findByTimeUnitAndExpirationAtBefore(TimeUnit.WEEK, LocalDateTime.now()).
+                        parallelStream().
                         forEach(ruleService::notifyExpiredRule);
             }catch (Exception e){
                 logger.error("Error in weekly expired rule,", e);
@@ -54,12 +60,13 @@ public class CheckExpiredRuleScheduler {
         }
     }
 
-    @Scheduled(cron = "0 0 0 1 * *")
+    @Scheduled(cron = "0 30 */23 * * *")
     public synchronized void checkMonthlyRules() {
-        lock = new ReentrantLock();
+        RLock lock = redissonClient.getLock("MONTHLY");
         if (lock.tryLock()) {
             try {
                 ruleRepository.findByTimeUnitAndExpirationAtBefore(TimeUnit.MONTH, LocalDateTime.now()).
+                        parallelStream().
                         forEach(ruleService::notifyExpiredRule);
             }catch (Exception e){
                 logger.error("Error in monthly expired rule,", e);
